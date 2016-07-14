bcdd2f15287acc17b7839710dee3d20f44edcf05
Noy <noyloy@gmail.com>
2016-07-14 18:51:24 +0300

    Merge branch 'gomri12/master' into noyloy
    # Conflicts:
    #	Podfile
    #	SmartHome/Base.lproj/Main.storyboard

--- Podfile
+++ Podfile
@@ -1,11 +1,22 @@
 # Uncomment this line to define a global platform for your project
+<<<<<<< HEAD
 platform :ios, '9.0'
+=======
+# platform :ios, '9.0'
+>>>>>>> gomri12/master
 
 target 'SmartHome' do
   # Comment this line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for SmartHome
+<<<<<<< HEAD
 
 pod 'Firebase'
+=======
+pod ‘Firebase’
+pod ‘Firebase/Auth’
+pod ‘Alamofire’
+
+>>>>>>> gomri12/master
 end
--- SmartHome/DeviceListTableViewController.swift
+++ SmartHome/DeviceListTableViewController.swift
@@ -7,20 +7,35 @@
 //
 import Foundation
 import UIKit
+import Firebase
 
 class DeviceListTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     // MARK: Properties
     @IBOutlet var deviceTableView: UITableView!
+<<<<<<< HEAD
+    var items: [Device] = [Device(location: "",name: "Bedroom TV",icon: UIImage(named: "airconditioner_icon")!),
+                           Device(location: "",name: "Livingroom TV",icon: UIImage(named: "airconditioner_icon")!),
+                           Device(location: "",name: "Kitchen TV",icon: UIImage(named: "airconditioner_icon")!),
+                           Device(location: "",name: "Front Lights",icon: UIImage(named: "airconditioner_icon")!),
+                           Device(location: "",name: "Bedroom Air Conditioner",icon: UIImage(named: "airconditioner_icon")!)]
+    
+    @IBAction func logoutBTN(sender: AnyObject) {
+        self.performSegueWithIdentifier("login", sender: nil)
+    }
+=======
     var items: [Device] = [Device(location: "",name: "Bedroom TV",icon: UIImage(named: "tv_icon")!),
                            Device(location: "",name: "Livingroom TV",icon: UIImage(named: "tv_icon")!),
                            Device(location: "",name: "Kitchen TV",icon: UIImage(named: "tv_icon")!),
                            Device(location: "",name: "Front Lights",icon: UIImage(named: "light_icon")!),
                            Device(location: "",name: "Bedroom Air Conditioner",icon: UIImage(named: "airconditioner_icon")!)]
+>>>>>>> origin/master
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.items.count
     }
     
+    @IBAction func addDeviceBTN(sender: AnyObject) {
+    }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell:DeviceTableCell = self.deviceTableView.dequeueReusableCellWithIdentifier("cell") as! DeviceTableCell
         
